a585a588fab6c67399698ea0f03fcba19548bbf5
jayanna G <84214745+Jayanth14072003@users.noreply.github.com>
2023-04-21 21:29:12 +0530

    Update config.py
    Custom caption added

--- config.py
+++ config.py
@@ -49,7 +49,7 @@ except ValueError:
 FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
-CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION",  "<b><code>{filename}<\code> /n/n𝐉𝐨𝐢𝐧 𝐎𝐮𝐫 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 - <a >𝐂𝐥𝐢𝐜𝐤 𝐇𝐞𝐫𝐞<\a><\b>")
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<b><code>{filename}</code> /n/n𝐉𝐨𝐢𝐧 𝐎𝐮𝐫 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 - <a href="http://t.me/Tamil_serials_contact_bot" >𝐂𝐥𝐢𝐜𝐤 𝐇𝐞𝐫𝐞</a></b>")
 
 #set True if you want to prevent users from forwarding files from bot
 PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
